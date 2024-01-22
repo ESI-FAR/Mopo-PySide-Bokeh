2eb6fd42e8afadc090dcb3ec381383fef744c63a
Ole Mussmann <gitlab+account@ole.mn>
2024-01-22 13:12:45 +0100

    allocate web view width dynamically

--- select_subset.py
+++ select_subset.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import time
 from typing import cast
 
 from bokeh.models import ColumnDataSource
@@ -29,6 +30,8 @@ class WorkerSignals(QObject):
     """A collection of communication queues."""
 
     radio_signal = Signal(str)
+    resize_signal = Signal(list)
+    viz_ready_signal = Signal(bool)
 
 
 class Worker(QRunnable):
@@ -44,21 +47,44 @@ class Worker(QRunnable):
         self.doc = None  # Bokeh canvas
         self.subset = "all"
 
-        # If `slider_value` changes, call the function `slider_changed`.
+        self.plot = None
+        self.plot_width = 0
+        self.plot_height = 0
+
         self.signals.radio_signal.connect(self.update)
+        self.signals.resize_signal.connect(self.update_size)
 
     def visualization(self, doc):
         """Attach Bokeh plot to canvas (doc)."""
         self.doc = doc
-        plot = mileage_tbl(self.model, self.source)
-        doc.add_root(plot)
+        self.plot = mileage_tbl(self.model, self.source)
+        doc.add_root(self.plot)
+        self.signals.viz_ready_signal.emit([0, 0])
 
     def update(self, subset: str):
         self.subset = subset
 
         # Communicating with Bokeh: queue `update_view` for the next iteration
         # of the Tornado server event loop.
-        self.doc.add_next_tick_callback(self.update_view)
+        if self.doc:
+            self.doc.add_next_tick_callback(self.update_view)
+
+    #def update_size(self, subset: str):
+    #    # Communicating with Bokeh: queue `update_view` for the next iteration
+    #    # of the Tornado server event loop.
+    #    self.doc.add_next_tick_callback(self.update_canvas_size)
+
+    def update_size(self, new_size: list[int]):
+        width, height = new_size
+        self.plot_width = width - 20  # TODO why is this needed?
+        self.plot_height = height
+
+        # Communicating with Bokeh: queue `update_canvas_size` for the next iteration
+        # of the Tornado server event loop.
+        if self.doc:
+            self.doc.add_next_tick_callback(self.update_canvas_size)
+        else:
+            print("no doc yet")
 
     async def update_view(self):
         """Update Bokeh plot."""
@@ -67,6 +93,11 @@ class Worker(QRunnable):
         else:
             data = self.model.df.query(f"manufacturer == '{self.subset}'")
         self.source.data = ColumnDataSource.from_df(data)
+
+    async def update_canvas_size(self):
+        """Update Bokeh canvas size."""
+        self.plot.width = self.plot_width
+        self.plot.height = self.plot_height
 
     def run(self):
         """Executed when Worker is started."""
@@ -78,33 +109,41 @@ class Worker(QRunnable):
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
         self.model = DF(mpg)
+
+        # run worker (bokeh viz and server) in background
+        self.threadpool = QThreadPool()
+        self.worker = Worker(self.model)
+        self.signals = self.worker.signals
+        self.signals.viz_ready_signal.connect(self.set_initial_size)
+        self.threadpool.start(self.worker)
 
         widget = QWidget()
 
-        wcols = QHBoxLayout()
+        self.vbox = QVBoxLayout()
+        self.vbox.addWidget(QLabel("Manufacturers"))
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(QLabel("Manufacturers"))
+        self.wcols = QHBoxLayout()
 
         # reset to all
         button = QRadioButton("all", widget)
         button.toggled.connect(self.update)
-        vbox.addWidget(button)
+        self.vbox.addWidget(button)
 
         for man in self.model.manufacturers:
             button = QRadioButton(man, widget)
             button.toggled.connect(self.update)
-            vbox.addWidget(button)
+            self.vbox.addWidget(button)
 
-        vbox.addStretch()  # flexible spacing at the bottom (can be 0)
-        wcols.addLayout(vbox, 1)
+        self.vbox.addStretch()  # flexible spacing at the bottom (can be 0)
+        self.wcols.addLayout(self.vbox, 1)
 
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl("http://localhost:5006/visualization"))
-        wcols.addWidget(self.browser, 4)
+        self.wcols.addWidget(self.browser, 4)
 
-        widget.setLayout(wcols)
+        widget.setLayout(self.wcols)
         self.setCentralWidget(widget)
 
         self.setWindowTitle("PoC: WebView based visualisation")
@@ -112,15 +151,17 @@ class MainWindow(QMainWindow):
 
         self.show()
 
-        # run worker (bokeh viz and server) in background
-        self.threadpool = QThreadPool()
-        self.worker = Worker(self.model)
-        self.threadpool.start(self.worker)
-
     def update(self):
         sender = cast(QRadioButton, self.sender())
         if sender.isChecked():
             self.worker.signals.radio_signal.emit(sender.text())
+
+    def set_initial_size(self, initial_size):
+        self.worker.signals.resize_signal.emit([self.browser.geometry().width(), self.browser.geometry().height()])
+
+    def resizeEvent(self, event):
+        self.worker.signals.resize_signal.emit([self.browser.geometry().width(), self.browser.geometry().height()])
+        QMainWindow.resizeEvent(self, event)
 
 
 def main():
